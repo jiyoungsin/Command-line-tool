fd6df86ea99e3b56e15bae3c68670d8bf6efdb1a
Paul Sin <jiyoungsin97@gmail.com>
2020-11-22 13:40:19 -0500

    include a useful commit message!

--- main_test.py
+++ main_test.py
@@ -1,16 +1,26 @@
 import unittest
 
-from functions import *
+from functions import get_response_code
 
 
 class MainTest(unittest.TestCase):
-    # def test_sum(self):
-    #     self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+    """
+    Testing CLI Command Repository
+    """
 
     def test_response(self):
-        self.assertEqual(get_response_code('https://www.google.com'), 200, "Should be 200")
-        self.assertEqual(get_response_code('https://www.google.com/incorrect_url'), 404, "Should be 404")
+        """
+        Testing for URL response code for status 200 and 404.
+        """
+        self.assertEqual(
+            get_response_code("https://www.google.com"), 200, "Should be 200"
+        )
+        self.assertEqual(
+            get_response_code("https://www.google.com/incorrect_url"),
+            404,
+            "Should be 404",
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
