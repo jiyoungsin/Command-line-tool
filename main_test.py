8c424ab215d9cb873a62a6c5f01cbeddefad292e
Devansh Shah <devzs6.98@gmail.com>
2020-11-27 21:09:19 -0500

    added 2 tests

--- main_test.py
+++ main_test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from functions import get_response_code
+from functions import get_urls_from_file
 
 
 class MainTest(unittest.TestCase):
@@ -19,6 +20,15 @@ class MainTest(unittest.TestCase):
             get_response_code("https://www.google.com/incorrect_url"),
             404,
             "Should be 404",
+        )
+        self.assertEqual(
+            type(get_response_code("https://www.google.com")), int, "should be a int"
+        )
+
+    def test_url_from_files(self):
+
+        self.assertEqual(
+            type(get_urls_from_file('urls.txt')), list, "should be a list"
         )
 
 
